8b92e6b2dab9597be1fead6ec7bc3b6361c3bb8e
Hüseyin Adeka <adekahuseyin@hotmail.com>
2022-01-04 14:34:47 +0300

    SecureLs eklendi
    SecureLs eklendi

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,16 +1,36 @@
 import { createStore } from 'redux';
 import authReducer from './authReducer';
+import SecureLS from 'secure-ls';
 
-const loggedInState = {
-    isloggedIn: true,
-    username: "Adekah",
-    displayName: "display1",
-    image: null,
-    password: 'P4ssword'
+const securels = new SecureLS();
+const getStateFromStorage = () => {
+    const hoaxAuth = securels.get('hoax-auth');
+
+    let stateInLocalStorage = {
+        isloggedIn: false,
+        username: undefined,
+        displayName: undefined,
+        image: undefined,
+        password: undefined
+    };
+
+    if (hoaxAuth) { return hoaxAuth }
+    return stateInLocalStorage;
 };
 
-const configureStore = () => {
-    return createStore(authReducer, loggedInState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const updateStateInStorage = newState => {
+    securels.set('hoax-auth', newState);
 }
+
+
+const configureStore = () => {
+    const store = createStore(authReducer, getStateFromStorage(), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+    store.subscribe(() => {
+
+        updateStateInStorage(store.getState());
+    })
+    return store;
+};
 
 export default configureStore;
